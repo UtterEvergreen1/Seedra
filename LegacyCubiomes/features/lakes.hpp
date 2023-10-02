e468548714f55f2291a91f43ff9f74196d532014
zugebot <zugebot2000@gmail.com>
2023-10-02 14:42:30 -0700

    lakes.hpp - fixed typo with scope of seed variable of generator being hidden in a previous commit, now uses correct getter

--- LegacyCubiomes/features/lakes.hpp
+++ LegacyCubiomes/features/lakes.hpp
@@ -8,7 +8,7 @@
 
 namespace Features {
     Pos3D waterLake(Generator* g, int chunkX, int chunkZ) {
-        uint64_t rng = getPopulationSeed(g->seed, chunkX, chunkZ);
+        uint64_t rng = getPopulationSeed(g->getWorldSeed(), chunkX, chunkZ);
         int biomeAt = g->getBiomeAt(1, (chunkX << 4) + 16, (chunkZ << 4) + 16);
         if (biomeAt == desert || biomeAt == desert_hills || nextInt(&rng, 4) != 0)
             return {0, 0, 0};
@@ -20,18 +20,18 @@ namespace Features {
     }
 
     Pos3D lavaLake(Generator* g, int chunkX, int chunkZ){
-        uint64_t rng = getPopulationSeed(g->seed, chunkX, chunkZ);
+        uint64_t rng = getPopulationSeed(g->getWorldSeed(), chunkX, chunkZ);
         int biomeAt = g->getBiomeAt(1, (chunkX << 4) + 16, (chunkZ << 4) + 16);
-        if ((biomeAt != desert && biomeAt != desert_hills) && nextInt(&rng, 4) == 0)//water lake in chunk
+        if ((biomeAt != desert && biomeAt != desert_hills) && nextInt(&rng, 4) == 0) // water lake in chunk
             return {0, 0, 0};
 
-        if (nextInt(&rng, 8) != 0)//no lava lake in chunk
+        if (nextInt(&rng, 8) != 0) // no lava lake in chunk
             return {0, 0, 0};
 
         int xPos = (chunkX << 4) + nextInt(&rng, 16) + 8;
         int yPos = nextInt(&rng, nextInt(&rng, 120) + 8);
         int zPos = (chunkZ << 4) + nextInt(&rng, 16) + 8;
-        if (yPos >= 63 && nextInt(&rng, 10) != 0)//no lava lake above sea level
+        if (yPos >= 63 && nextInt(&rng, 10) != 0) // no lava lake above sea level
             return {0, 0, 0};
 
         return {xPos, yPos, zPos};
