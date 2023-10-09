8ed0e8980cf4904085072a9ce330d9632fc48aca
zugebot <zugebot2000@gmail.com>
2023-10-09 12:53:54 -0700

    Added file LegacyCubiomes/utils/picture.hpp has function writeBiomesToImage(Generator* g, std::string file_directory). pass the directory of where you want it to put the image biome map.

--- main1.cpp
+++ main1.cpp
@@ -1,23 +1,62 @@
 
-#include <fstream>
 #include <iostream>
+#include <vector>
+#include <fstream>
+
+
 
 #include "LegacyCubiomes/structures/include.hpp"
-#include "LegacyCubiomes/enchants/include.hpp"
-
+#include "LegacyCubiomes/enchants/enchantmentHelper.hpp"
 #include "LegacyCubiomes/cubiomes/generator.hpp"
-#include "LegacyCubiomes/chunk_generator/biome.hpp"
 
+#include "LegacyCubiomes/chunk_generator/biome.hpp"
+#include "LegacyCubiomes/cubiomes/picture.hpp"
 
 
 int main(int argc, char* argv[]) {
-    // setup
-    Biome::registerBiomes();
-    EnchantmentHelper::setup(CONSOLE::WIIU, LCEVERSION::AQUATIC);
-    Generator g(CONSOLE::WIIU, LCEVERSION::AQUATIC, 12498783792879328, BIOMESCALE::SMALL, WORLDSIZE::CLASSIC);
+    std::string dir = R"(C:\Users\Jerrin\CLionProjects\LegacyCubiomes\)";
 
-    // code
-    generation::Mineshaft gen{};
-    gen.generate(g.getWorldSeed(), 0, 0);
-    std::cout << gen.pieceArraySize << std::endl;
+    auto console = CONSOLE::PS3;
+    auto version = LCEVERSION::AQUATIC;
+    auto size = WORLDSIZE::CLASSIC;
+    auto scale = BIOMESCALE::LARGE;
+    auto seed = 1185706985;
+
+    Biome::registerBiomes();
+    EnchantmentHelper::setup(console, version);
+    Generator g(console, version, size, scale);
+    g.applyWorldSeed(seed);
+
+    writeBiomesToImage(&g, dir);
+
+    return 0;
 }
+
+
+
+
+/*
+   uint64_t rng;
+   int passes = 1;
+   Pos2D pos = {0, 0};
+   auto biome = ocean;
+
+   Pos2D biomePos = g.locateBiome(
+       pos,
+       4,
+       biome,
+       &rng,
+       &passes
+       );
+
+   std::cout << biomePos << std::endl;
+
+
+   while (true) {
+       // g.incrementSeed();
+       // Pos2DVec_t pos2DVec = Placement::BuriedTreasure::getAllPositions(&g);
+       // if (pos2DVec.empty()) continue;
+       // for (Pos2D pos : pos2DVec) {}
+
+       break;
+   }*/
